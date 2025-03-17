29af13e9095ae39f1f6f1667c55cd65a68bec43a
AdiAnteater <150882784+AdiAnteater@users.noreply.github.com>
2025-03-16 20:38:13 -0700

    Completed ladder main

---
+++ src/ladder_main.cpp
@@ -0,0 +1,18 @@
+#include "ladder.c"
+
+int main()
+{
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+
+    string start, end;
+    cout << "Enter start word: ";
+    cin >> start;
+    cout << "Enter end word: ";
+    cin >> end;
+
+    vector<string> ladder = generate_word_ladder(start, end, word_list);
+    print_word_ladder(ladder);
+
+    return 0;
+}
